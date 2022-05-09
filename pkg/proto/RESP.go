9b1a80306a460e6fac87f05bced0e6d6404b8908
crane <artisticbones@163.com>
2022-05-09 23:40:14 +0800

    update(RESP): Redis Serialization Protocol

--- pkg/proto/RESP.go
+++ pkg/proto/RESP.go
@@ -1,5 +1,10 @@
 package proto
 
+import (
+	"github.com/artisticbones/redis-learn/internal/redis"
+	"io"
+)
+
 /**
  * RESP 是一个二进制安全的文本协议，工作于 TCP 协议上。
  * RESP 以行作为单位，客户端和服务器发送的命令或数据一律以 \r\n （CRLF）作为换行符。
@@ -12,3 +17,20 @@ package proto
  ** 字符串(Bulk String): 二进制安全字符串, 比如 get 等命令的返回值。										|	以"$"开始
  ** 数组(Array, 又称 Multi Bulk Strings): Bulk String 数组，客户端发送指令以及 lrange 等命令响应的格式。	|	以"*"开始
  */
+
+type Payload struct {
+	Data redis.Reply
+	Err  error
+}
+
+// ParseStream 通过 io.Reader 读取数据并将结果通过 channel 将结果返回给调用者
+// 流式处理的接口适合供客户端/服务端使用
+func ParseStream(reader io.Reader) <-chan *Payload {
+	ch := make(chan *Payload)
+	go parse0(reader, ch)
+	return ch
+}
+
+func parse0(reader io.Reader, ch chan<- *Payload) {
+
+}
